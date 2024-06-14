9c5aae33a62059efff0becd9348f5124acb95fe0
sushma <sushmaacharya984@gmail.com>
2024-06-15 00:13:28 +0545

    ins

--- InsertionSort/main.cpp
+++ InsertionSort/main.cpp
@@ -5,30 +5,37 @@
 
 int main() {
     // Insertion sort
-    int array_size = 20000;
-
+    int array_size = 1000;
+    
     // Seed with a real random value
     std::random_device rd;
     // Initialize Mersenne Twister pseudo-random number generator
     std::mt19937 gen(rd());
     // Define the range for random numbers
-    std::uniform_int_distribution<> dis(0,20000);
-
+    std::uniform_int_distribution<> dis(0, 1000);
+    
     int random_numbers[array_size];
     // Generate random numbers
     for (int i = 0; i < array_size; i++) {
         random_numbers[i] = dis(gen);
     }
-
+    
+    // Generated random numbers
+    std::cout << "Generated random numbers:\n";
+    printArray(random_numbers, array_size);
+    
     // Measure time taken by insertion sort
     auto start = std::chrono::high_resolution_clock::now();
     // Pass generated random numbers to insertion sort function
     insertionSort(random_numbers, array_size);
     auto end = std::chrono::high_resolution_clock::now();
-
+    
     std::chrono::duration<double, std::milli> insertion_sort_duration = end - start;
-
+    
+    std::cout << "\nSorted array using insertion sort:\n";
+    printArray(random_numbers, array_size);
+    
     std::cout << "Time taken by insertion sort: " << insertion_sort_duration.count() << " milliseconds\n";
-
+    
     return 0;
 }
