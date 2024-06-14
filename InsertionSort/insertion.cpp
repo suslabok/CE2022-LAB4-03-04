ae1ad274018637b3d5d4371db6db8d9c8cc3a508
sushma <sushmaacharya984@gmail.com>
2024-06-14 22:42:14 +0545

    insertion

---
+++ InsertionSort/insertion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "insertion.h"
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Function to print the array
+void printArray(const int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
---
+++ InsertionSort/insertion.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Function prototype for insertion sort
+void insertionSort(int arr[], int n);
+
+// Function prototype to print the array
+void printArray(const int arr[], int n);
